5dd7af930ecb79f5d9646c0f02c0ea8b182c7938
Gohar Aslanyan <Gohar@Gohars-MacBook-Pro.local>
2022-01-13 14:20:23 -0800

    style changes

--- app/pages/Works.php
+++ app/pages/Works.php
@@ -4,10 +4,10 @@ function pageWorks(){
 <div class="row-ga">
    
 <div class="description">
-        <div class="list-view-name">Impression Web Studio</div>
-        <div class="list-view-undertext">Logo, Website Design, Business Card, Brochure</div>
+        <div class="list-view-name flex-order1">Impression Web Studio</div>
+        <div class="list-view-undertext flex-order2">Logo, Website Design, Business Card, Brochure</div>
         
-        <button type="button" id="visit" class="ga-btn-visit">visit website</button>
+        <button type="button" id="visit" class="ga-btn-visit flex-order3">visit website</button>
         
         <p>In May 2008 with 2 of my friends we founded a web studio based in Yerevan, Armenia. It took us months to do lots of research and to come up with the meticulously planned strategies, structure and architecture of the website and services we’d provide. Since we were very low on budget, we planned everything very carefully using as much of our own recourses as we could. This included naming, branding, photography, copywriting, designing, coding and even using my own child and my friend as models.
 In the end we were very proud of our product. It was very versatile, modern and easy to use.
@@ -15,16 +15,16 @@ The company started to grow and gain profits.
 It still exits and even though I moved to US in 2011, I’m still wholeheartedly attached to this project like to my own child.
         </p>
         
-        <div class="list-view-name"><a href="">Next Project ></a></div>
-        <div class="list-view-name"><a href="">< Previous Project ></a></div>    
+        <div class="list-view-name flex-order4"><a href="">Next Project ></a></div>
+        <div class="list-view-name flex-order5"><a href="">< Previous Project ></a></div>    
 </div>
     
 <div class="works">
-    <div class="list-view-name">1/2</div>
-    <div class="list-view-name">Home page with a dynamic carousel</div>
+    <div class="list-view-name flex-order6">1/2</div>
+    <div class="list-view-name flex-order7">Home page with a dynamic carousel</div>
   
     <div class="works-order"><a href="prev project"><img class="works-arrows" src="/img/content/portfolio graphics/left-arrow.png"></a></div>  
-    <img class="works-image works-order" src="/img/content/single-view/impression/1-impression-single-view.jpg">
+    <img class="works-image works-order flex-order8" src="/img/content/single-view/impression/1-impression-single-view.jpg">
     <div class="works-order"><a href="prev project"><img class="works-arrows" src="/img/content/portfolio graphics/right-arrow.png"></a></div> 
     
 </div>  
