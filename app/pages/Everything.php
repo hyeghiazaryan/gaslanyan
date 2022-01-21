deabcb5d671ba7e5cd458b28cf14c7d9566ab3a6
Hrachya Yeghiazaryan <hrach@Hrachyas-MacBook-Air.local>
2022-01-21 14:14:07 -0800

    logos and everything

--- app/pages/Everything.php
+++ app/pages/Everything.php
@@ -4,18 +4,19 @@ function pageEverything(){
 <div class="row-ga" >
     
         <div class="list-view-style">
-            <a href="http://www.impressionwebstudio.com/" target="_blank">
+            <a href="/img/everything/1-armen-bc.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/1-armen-bc.jpg" alt="Piano Tuning And Maintenance Business Card"/>
                     <div class="list-view-name">Piano Tuning And Maintenance</div>
             </a>    <!-- comment -->
         </div>
         <div class="list-view-style">
-            <a href="http://www.impressionwebstudio.com/" target="_blank">
+            <a href="/img/everything/2-arthur-bc.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/2-arthur-bc.jpg" alt="CPA Financial Services Business Card"/>
                     <div class="list-view-name">CPA Financial Services</div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/3-karen-bc.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/3-karen-bc.jpg" alt="Yerevan Opera Theater Grand Conductor Business Card"/>
                     <div class="list-view-name">Yerevan Opera Theater Grand Conductor</div>
             </a>    <!-- comment -->
@@ -24,17 +25,20 @@ function pageEverything(){
     
     
     
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/4-nazik.jpeg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/4-nazik.jpeg" alt="Homemade Armenian Pastry Business Card"/>
                     <div class="list-view-name">Homemade Armenian Pastry</div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/5-self-love-first.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/5-self-love-first.jpg" alt="Self Love First Life And Personal Coach Business Card"/>
                     <div class="list-view-name">Self Love First Life And Personal Coach</div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style" ><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style" >
+            <a href="/img/everything/6-lena-bc.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img  src="/img/everything/6-lena-bc.jpg" alt="Lena Aslanyan Photography Business Card"/>
                     <div class="list-view-name">Lena Aslanyan Photography</div>
             </a>    <!-- comment -->
@@ -42,16 +46,20 @@ function pageEverything(){
 
     
     
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/7-happy-pirate.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/7-happy-pirate.jpg" alt="Illustration Of A Pirate with A Happy Face Expression"/>
                     <div class="list-view-name">A Pirate With A Happy Face</div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/8-surprised-pirate.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img  src="/img/everything/8-surprised-pirate.jpg" alt="Illustration Of A Pirate with A Surprised Face Expression"/>
                     <div class="list-view-name">A Pirate With A Surprised Face</div>
+            </a>
         </div>
-        <div class="list-view-style" ><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style" >
+            <a href="/img/everything/9-angry-pirate.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/9-angry-pirate.jpg" alt="Illustration Of A Pirate with An Angry Face Expression"/>
                     <div class="list-view-name">A Pirate With An Angry Face</div>
             </a>    <!-- comment -->
@@ -60,16 +68,19 @@ function pageEverything(){
 
     
     
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/10-florida.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/10-florida.jpg" alt="Florida Waterside Landscape Watercolor Painting"/>
                     <div class="list-view-name">Florida Waterside Landscape Watercolor Painting</div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/11-alien.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img  src="/img/everything/11-alien.jpg" alt="Alien Picking From The Underground Illustration"/>
                     <div class="list-view-name">An Alien Peeking From The Underground Illustration</div>
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/12-vallarta.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/12-vallarta.jpg" alt="Puerto Vallarta Mexican Hotel View Illustration"/>
                     <div class="list-view-name">Puerto Vallarta Mexican Hotel View Illustration</div>
             </a>    <!-- comment -->
@@ -78,22 +89,35 @@ function pageEverything(){
 
     
     
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/13-portfolio.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/13-portfolio.jpg" alt="My Website’s Top Illustration Initial Design"/>
                     <div class="list-view-name">My Website’s Top Illustration Initial Sketch </div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/14-mushtakik.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img  src="/img/everything/14-mushtakik.jpg" alt="A Children Book Illustration Suggestion"/>
                     <div class="list-view-name">A Children Book Illustration Suggestion</div>
+            </a>
         </div>
-        <div class="list-view-style" ><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/everything/15-mushtakik.jpg" data-toggle="lightbox" data-gallery="everything-gallery" data-type="image">
                     <img src="/img/everything/15-mushtakik.jpg" alt="A Children Book Illustration Suggestion II"/>
                     <div class="list-view-name">A Children Book Illustration Suggestion II</div>
             </a>    <!-- comment -->
         </div>   
    
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js"></script>
+<script type="text/javascript">
+                    $(document).ready(function ($) {
+                        $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+            event.preventDefault();
+            $(this).ekkoLightbox();
+        });
+                    });
+</script>
 
 <!--Columns end-->
 <?php } ?>
--- app/pages/Logos.php
+++ app/pages/Logos.php
@@ -4,18 +4,19 @@ function pageLogos(){
 <div class="row-ga" >
     
         <div class="list-view-style">
-            <a href="http://www.impressionwebstudio.com/" target="_blank">
+            <a href="/img/logos/1-impression.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img src="/img/logos/1-impression.jpg" alt="Impression Web Studio Logo"/>
                     <div class="list-view-name">Impression Web Studio</div>
             </a>    <!-- comment -->
         </div>
         <div class="list-view-style">
-            <a href="http://www.impressionwebstudio.com/" target="_blank">
+            <a href="/img/logos/2-ograda.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img src="/img/logos/2-ograda.jpg" alt="Ograda Local Farmers Market And Tapas Bar Logo"/>
                     <div class="list-view-name">Ograda Local Farmers Market And Tapas Bar </div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/logos/3-equinox.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img src="/img/logos/3-equinox.jpg" alt="Equinox Books Publishing For Children Logo"/>
                     <div class="list-view-name">Equinox Books Publishing For Children</div>
             </a>    <!-- comment -->
@@ -23,17 +24,20 @@ function pageLogos(){
 
     
     
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/logos/4-lesekroken.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img src="/img/logos/4-lesekroken.jpg" alt="Lesekroken Stories For Immigrant Kids Logo"/>
                     <div class="list-view-name">Lesekroken Stories For Immigrant Kids</div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/logos/5-pag.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img src="/img/logos/5-pag.jpg" alt="PAG Fine Homes Construction Company Logo"/>
                     <div class="list-view-name">PAG Fine Homes Construction Company</div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style" ><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style" >
+            <a href="/img/logos/6-sheboygan.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img  src="/img/logos/6-sheboygan.jpg" alt="Sheboygan Area School District In Wisconsin Logo"/>
                     <div class="list-view-name">Sheboygan Area School District In Wisconsin</div>
             </a>    <!-- comment -->
@@ -41,40 +45,56 @@ function pageLogos(){
 
     
     
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/logos/7-filofino.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img src="/img/logos/7-filofino.jpg" alt="Filofino Italian Fine Wool Clothing Company Logo"/>
                     <div class="list-view-name">Filofino Italian Fine Wool Clothing Company</div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/logos/8-wild-and-true.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img  src="/img/logos/8-wild-and-true.jpg" alt="Wild And True Outdoor Vloggers Logo"/>
                     <div class="list-view-name">Wild And True Outdoor Vloggers</div>
+            </a>
         </div>
-        <div class="list-view-style" ><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style" >
+            <a href="/img/logos/9-spring.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img src="/img/logos/9-spring.jpg" alt="SPRing Strategic PR Company Logo"/>
                     <div class="list-view-name">SPRing Strategic PR Company</div>
-            </a>    <!-- comment -->
+            </a>
         </div>   
    
 
     
     
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/logos/10-christian.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img src="/img/logos/10-christian.jpg" alt="1900 Christian Street’s Building Logo"/>
                     <div class="list-view-name">1900 Christian Street’s Building</div>
             </a>    <!-- comment -->
         </div>
-        <div class="list-view-style"><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style">
+            <a href="/img/logos/11-hux.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img  src="/img/logos/11-hux.jpg" alt="HUX Office Chairs And Furniture Company Logo"/>
                     <div class="list-view-name">HUX Office Chairs And Furniture Company</div>
+            </a>
         </div>
-        <div class="list-view-style" ><a href="http://www.impressionwebstudio.com/" target="_blank">
+        <div class="list-view-style" >
+            <a href="/img/logos/12-omorose.jpg" data-toggle="lightbox" data-gallery="logo-gallery" data-type="image">
                     <img src="/img/logos/12-omorose.jpg" alt="Omorose Clothing Company Logo"/>
                     <div class="list-view-name">Omorose Clothing Company</div>
             </a>    <!-- comment -->
         </div>   
    
 </div>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js"></script>
+<script type="text/javascript">
+                    $(document).ready(function ($) {
+                        $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+            event.preventDefault();
+            $(this).ekkoLightbox();
+        });
+                    });
+</script>
 <!--Columns end-->
 <?php } ?>
