f37c3e0f98effcae9d6694dda1ca451caa6927d0
Hrachya Yeghiazaryan <hrach@Hrachyas-MacBook-Air.local>
2022-01-23 14:26:08 -0800

    mobile controls

--- app/pages/Websites.php
+++ app/pages/Websites.php
@@ -103,6 +103,17 @@ function pageWebsites(){
                     </div>
                     <?php }?>
                 </div>
+                
+                <!-- Left and right controls -->
+                <a class="left carousel-control mobile-show" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control mobile-show" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                
 
             </div>              
             <div class="works-order mobile-arrows"><a href="#myCarousel" data-slide="next"><img class="works-arrows" src="/img/content/right-arrow.png"></a></div>
